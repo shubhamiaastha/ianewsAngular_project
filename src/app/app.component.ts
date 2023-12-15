6885d9bb15638b1c5bacb977557a123c68963b53
shubhamiaastha <shubham.dubey@iaastha.com>
2023-12-15 17:27:23 +0530

    UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss'  
 })
 export class AppComponent {
   constructor(private router: Router) {}
