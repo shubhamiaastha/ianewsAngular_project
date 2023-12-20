f9a580f9b599bec4f9a43d4fdf1cf926b5ee023b
shubhamiaastha <shubham.dubey@iaastha.com>
2023-12-20 14:01:26 +0530

    NEW UI

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -52,8 +52,68 @@
 //   }
 // }
 
+// import { Component, OnInit } from '@angular/core';
+// import { NewsDataService } from "../services/news-data.service";
+
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.scss']
+// })
+// export class UserComponent implements OnInit {
+//   cardDataArray: any[] = [];
+//   currentPage = 1;
+//   isLoading = false;
+//   isHoveredIndex: number = -1;
+
+//   constructor(private newData: NewsDataService) {}
+
+//   ngOnInit() {
+//     this.loadPage();
+//   }
+
+//   async loadPage() {
+//     this.isLoading = true;
+
+//     try {
+//       const data: any = await this.newData.news(this.currentPage).toPromise();
+
+//       console.log('API Response for Page', this.currentPage, ':', data);
+
+//       if (data && data.data && Array.isArray(data.data.news)) {
+//         this.cardDataArray = data.data.news;
+//         console.warn(this.cardDataArray);
+//       } else {
+//         console.error('Invalid data Response:', data);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     } finally {
+//       this.isLoading = false;
+//     }
+//   }
+
+//   setHoveredIndex(index: number) {
+//     this.isHoveredIndex = index;
+//   }
+
+//   async onNextClick() {
+//     this.currentPage++;
+//     await this.loadPage();
+//   }
+
+//   async onPreviousClick() {
+//     if (this.currentPage > 1) {
+//       this.currentPage--;
+//       await this.loadPage();
+//     }
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { NewsDataService } from "../services/news-data.service";
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user',
@@ -62,7 +122,7 @@ import { NewsDataService } from "../services/news-data.service";
 })
 export class UserComponent implements OnInit {
   cardDataArray: any[] = [];
-  currentPage = 1;
+  currentPage = 20;
   isLoading = false;
   isHoveredIndex: number = -1;
 
@@ -76,15 +136,19 @@ export class UserComponent implements OnInit {
     this.isLoading = true;
 
     try {
-      const data: any = await this.newData.news(this.currentPage).toPromise();
+      const data: any = await this.newData.news(this.currentPage)
+        .pipe(
+          delay(500) // Adjust the delay time as needed
+        )
+        .toPromise();
 
-      console.warn(data);
+      console.log('API Response for Page', this.currentPage, ':', data);
 
       if (data && data.data && Array.isArray(data.data.news)) {
         this.cardDataArray = data.data.news;
-        console.warn(this.cardDataArray);
+        console.log('Card Data Array:', this.cardDataArray);
       } else {
-        console.error('Invalid data Response:', data);
+        console.error('Invalid data response:', data);
       }
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -109,6 +173,8 @@ export class UserComponent implements OnInit {
     }
   }
 }
+
+
 
 
 
